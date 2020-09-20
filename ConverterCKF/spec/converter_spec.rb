69a4018d5a65569156a49a4f56b5f845c058c46c
scrubi19 <scrubi.memo@yandex.ru>
2020-09-20 22:52:58 +0700

    rubocop fix

--- ConverterCKF/spec/converter_spec.rb
+++ ConverterCKF/spec/converter_spec.rb
@@ -4,30 +4,30 @@ require 'rspec'
 require_relative '../lib/converter_ckf'
 
 Rspec.describe ConverterCKF do
-	describe 'converter_check' do
-		it 'From C to F' do
-			expect(ConverterCKF.convert(10,'C','F')).to eq 50
-		end
+  describe 'converter_check' do
+    it 'From C to F' do
+      expect(ConverterCKF.convert(10, 'C', 'F')).to eq 50
+    end
 
-		it 'From C to K' do
-			expect(ConverterCKF.convert(10,'C','K')).to eq 283
-		end
+    it 'From C to K' do
+      expect(ConverterCKF.convert(10, 'C', 'K')).to eq 283
+    end
 
-		it 'From F to C' do
-			expect(ConverterCKF.convert(10,'F','C')).to eq -13
-		end
+    it 'From F to C' do
+      expect(ConverterCKF.convert(10, 'F', 'C')).to eq - 13
+    end
 
-		it 'From F to K' do
-			expect(ConverterCKF.convert(10,'F','K')).to eq 265
-		end
+    it 'From F to K' do
+      expect(ConverterCKF.convert(10, 'F', 'K')).to eq 265
+    end
 
-		it 'From K to C' do
-			expect(ConverterCKF.convert(10,'K','C')).to eq -263
-		end
+    it 'From K to C' do
+      expect(ConverterCKF.convert(10, 'K', 'C')).to eq - 263
+    end
 
-		it 'From K to F' do
-			expect(ConverterCKF.convert(210,'K','F')).to eq -81
-		end
-	end
+    it 'From K to F' do
+      expect(ConverterCKF.convert(210, 'K', 'F')).to eq - 81
+    end
+  end
 
 end
