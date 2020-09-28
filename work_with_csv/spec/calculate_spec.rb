50dfc27dec8992b459df678ee197539fee73bac1
scrubi19 <scrubi.memo@yandex.ru>
2020-09-28 12:28:30 +0700

    fix rspec and name of directory

---
+++ work_with_csv/spec/calculate_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../lib/csv_work'
+
+RSpec.describe CSVcalc do
+  describe '.csv calculate' do
+    data = CSVcalc.new('spec/test.csv')
+    context 'Search min' do
+      it { expect(data.csv_min[0]).to eq 'Крымский федеральный округ' }
+    end
+
+    context 'Search max' do
+      it { expect(data.csv_max[0]).to eq 'Амурская область' }
+    end
+
+    context 'Calculate mean' do
+      it { expect(data.csv_calculate_mean).to eq 57.0 }
+    end
+
+    context 'Calculate correct sample variance' do
+      it { expect(data.csv_correct_sample_variance).to eq 1545.8333333333333 }
+    end
+  end
+end
